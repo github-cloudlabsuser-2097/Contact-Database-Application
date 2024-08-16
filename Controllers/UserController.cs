082c92c140fcb51199f0dd6029b901309f0a6f4d
Your Name <you@example.com>
2024-08-16 21:41:12 +0000

    commit

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,66 +1,87 @@
-using CRUD_application_2.Models;
+using System.Collections.Generic;
 using System.Linq;
+using System.Web;
+using System.Web.Optimization;
 using System.Web.Mvc;
- 
-namespace CRUD_application_2.Controllers
+
+namespace YourNamespace.Controllers
 {
     public class UserController : Controller
     {
-        public static System.Collections.Generic.List<User> userlist = new System.Collections.Generic.List<User>();
+        // In-memory list of users for demonstration purposes
+        private static List<User> userList = new List<User>
+        {
+            new User { Id = 1, Name = "John Doe", Email = "john@example.com" },
+            new User { Id = 2, Name = "Jane Smith", Email = "jane@example.com" }
+        };
+
         // GET: User
         public ActionResult Index()
         {
-            // Implement the Index method here
+            return View(userList);
         }
- 
-        // GET: User/Details/5
-        public ActionResult Details(int id)
-        {
-            // Implement the details method here
-        }
- 
-        // GET: User/Create
-        public ActionResult Create()
-        {
-            //Implement the Create method here
-        }
- 
-        // POST: User/Create
-        [HttpPost]
-        public ActionResult Create(User user)
-        {
-            // Implement the Create method (POST) here
-        }
- 
+
         // GET: User/Edit/5
         public ActionResult Edit(int id)
         {
-            // This method is responsible for displaying the view to edit an existing user with the specified ID.
-            // It retrieves the user from the userlist based on the provided ID and passes it to the Edit view.
+            var user = userList.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
         }
- 
+
         // POST: User/Edit/5
         [HttpPost]
         public ActionResult Edit(int id, User user)
         {
-            // This method is responsible for handling the HTTP POST request to update an existing user with the specified ID.
-            // It receives user input from the form submission and updates the corresponding user's information in the userlist.
-            // If successful, it redirects to the Index action to display the updated list of users.
-            // If no user is found with the provided ID, it returns a HttpNotFoundResult.
-            // If an error occurs during the process, it returns the Edit view to display any validation errors.
+            var existingUser = userList.FirstOrDefault(u => u.Id == id);
+            if (existingUser == null)
+            {
+                return HttpNotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                existingUser.Name = user.Name;
+                existingUser.Email = user.Email;
+                return RedirectToAction("Index");
+            }
+            return View(user);
         }
- 
+
         // GET: User/Delete/5
         public ActionResult Delete(int id)
         {
-            // Implement the Delete method here
+            var user = userList.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
         }
- 
+
         // POST: User/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
         {
-            // Implement the Delete method (POST) here
+            var user = userList.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+
+            userList.Remove(user);
+            return RedirectToAction("Index");
         }
+    }
+
+    // User model for demonstration purposes
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
     }
 }
